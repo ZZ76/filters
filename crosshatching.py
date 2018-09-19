2b01e4c54eca161f722806ab9f6310ea7371443a
ZZ76 <ZZ76@users.noreply.github.com>
2018-09-19 19:26:51 +1200

    edited crosshatching

--- crosshatching.py
+++ crosshatching.py
@@ -31,6 +31,8 @@ def lines(img, code=None, step=12):
         tantheta = 2.4142
         for i in range(0, int(w+h*cotheta), step):
             l = cv2.line(l, (i, 0), (0, int(i*tantheta)), black)
+    else:
+        pass   # empty
     return l
 
 
@@ -41,26 +43,9 @@ def tsh(img, stage=None, Numberoftsh=None, equalizeHist=False):
         pass
     else:
         img_gray = cv2.equalizeHist(img_gray, img_gray)
-    _, th = cv2.threshold(img_gray, 255-int(((stage+1)/Numberoftsh)*255), 255, type)
+    _, th = cv2.threshold(img_gray, 255-int(((stage)/Numberoftsh)*255), 255, type)
     th = cv2.cvtColor(th, cv2.COLOR_GRAY2BGR)
-    return th
-
-def tsh2(img, code=None, equalizeHist=False):
-    type = cv2.THRESH_BINARY
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    if equalizeHist == False:
-        pass
-    else:
-        img_gray = cv2.equalizeHist(img_gray, img_gray)
-    if code == 0:
-        _, th = cv2.threshold(img_gray, 205, 255, type)
-    elif code == 1:
-        _, th = cv2.threshold(img_gray, 153, 255, type)
-    elif code == 2:
-        _, th = cv2.threshold(img_gray, 102, 255, type)
-    elif code == 3:
-        _, th = cv2.threshold(img_gray, 51, 255, type)
-    th = cv2.cvtColor(th, cv2.COLOR_GRAY2BGR)
+    print(255-int(((stage+1)/Numberoftsh)*255), 255)
     return th
 
 
@@ -69,6 +54,7 @@ def crosshatching(img, Numberoftsh=None, equalizeHist=False):
     dst2[:] = 255
     for i in range(Numberoftsh):
         mask = lines(img, code=seqline[i], step=8)
+        print(i, seqline[i])
         th = tsh(img, stage=i, Numberoftsh=Numberoftsh, equalizeHist=equalizeHist)
         dst = cv2.addWeighted(mask, 1, th, 1, 0)
         dst = cv2.bitwise_and(dst, dst2)
@@ -76,34 +62,11 @@ def crosshatching(img, Numberoftsh=None, equalizeHist=False):
     return dst2
 
 
-def crosshatching2(img):
-    mask = lines(img, code=0, step=8)
-    th = tsh(img, code=0)
-    dst = cv2.addWeighted(mask, 1, th, 1, 0)
-    dst2 = dst
-    mask = lines(img, code=4)
-    th = tsh(img, code=1)
-    dst = cv2.addWeighted(mask, 1, th, 1, 0)
-    dst = cv2.bitwise_and(dst, dst2)
-    dst2 = dst
-    mask = lines(img, code=3)
-    th = tsh(img, code=2)
-    dst = cv2.addWeighted(mask, 1, th, 1, 0)
-    dst = cv2.bitwise_and(dst, dst2)
-    dst2 = dst
-    mask = lines(img, code=5)
-    th = tsh(img, code=3)
-    dst = cv2.addWeighted(mask, 1, th, 1, 0)
-    dst = cv2.bitwise_and(dst, dst2)
-    dst2 = dst
-    return dst
-
-def main(img):
+def main(img, Numberoftsh = 7, equalizeHist=True):
     global w, h, seqline
     h, w, _ = img.shape
-    seqline = (0, 4, 3, 5, 1, 2)
-    Numberoftsh = 6
-    dst = crosshatching(img, Numberoftsh=Numberoftsh)
+    seqline = (-1, 0, 4, 3, 5, 1, 2)
+    dst = crosshatching(img, Numberoftsh=Numberoftsh, equalizeHist=equalizeHist)
     #dst = cv2.resize(dst, (int(w/2), int(h/2)))
     cv2.imshow('dst', dst)
     cv2.waitKey(0)
@@ -126,7 +89,7 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         pass
     else:
-        img = cv2.imread('kim.jpg')
+        img = cv2.imread('lena.jpg')
         main(img)
 else:
     pass
